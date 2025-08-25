65d3338d59cd6880684c946631f91a0cd5473a4d
JESINTHA SHARON S <167150534+JESINTHASHARON@users.noreply.github.com>
2025-08-25 15:06:20 +0530

    Update newRequest.js

--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-  baseURL: "http://localhost:2005/api/",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
